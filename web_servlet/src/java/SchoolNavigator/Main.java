006c9a01cb38d0f074f15cac11c0b6b79dca4ae8
zethra <jediben97@gmail.com>
2015-06-05 10:51:51 -0400

    Working on web page

--- web_servlet/src/java/SchoolNavigator/Main.java
+++ web_servlet/src/java/SchoolNavigator/Main.java
@@ -1,19 +1,14 @@
 package SchoolNavigator;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="SchoolNavigator", urlPatterns={"/nav"})
+@WebServlet(name="SchoolNavigator", urlPatterns={"/"})
 public class Main  extends HttpServlet{
     
     @Override
@@ -24,23 +19,9 @@ public class Main  extends HttpServlet{
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
-        String serverAddress = "localhost";
-        Socket s = new Socket(serverAddress, 1234);
-        System.out.printf("Connected to: %s\n", s.getLocalAddress());
-        PrintWriter output = new PrintWriter(s.getOutputStream(), true);
-        output.println("100,918");
-        System.out.println("Sent: 100,918");
-        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String answer = "";
-        String raw = "";
-        while (true) {
-            raw = input.readLine();
-            if(raw.equals("END"))
-                break;
-            answer += raw + "<br/>";
-        }
+        
+        out.print(request.getContextPath());
         
         
-        out.print(answer);
     }
 }
---
+++ web_servlet/src/java/SchoolNavigator/Navigator.java
@@ -0,0 +1,50 @@
+package SchoolNavigator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="SchoolNavigator", urlPatterns={"/nav"})
+public class Navigator  extends HttpServlet{
+    
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+              throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String serverAddress = "localhost";
+        try {
+            Socket s = new Socket(serverAddress, 1234);
+            System.out.printf("Connected to: %s\n", s.getLocalAddress());
+            PrintWriter output = new PrintWriter(s.getOutputStream(), true);
+            output.println("100,918");
+            System.out.println("Sent: 100,918");
+            BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String answer = "";
+            String raw;
+            while (true) {
+                raw = input.readLine();
+                if(raw.equals("END"))
+                    break;
+                answer += raw + "<br/>";
+            }
+            out.print(answer);
+        } catch(ConnectException e) {
+            out.print("Cannot connect to map server");
+        }
+        
+        
+        
+    }
+}
