4a3490f979eae00e64b636652940cebfabd7a14e
zethra <jediben97@mgail.com>
2015-06-11 10:25:20 -0400

    Working on adding service

---
+++ map_server/src/SchoolNavigator/ServiceRunner.java
@@ -0,0 +1,39 @@
+package SchoolNavigator;
+
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonInitException;
+
+public class ServiceRunner implements Daemon{
+    private static Server server;
+
+    public static void main(String[] args) {
+        server = new Server(1234);
+        new Thread(server);
+    }
+
+    @Override
+    public void init(DaemonContext dc) throws DaemonInitException, Exception {
+        System.out.println("initializing ...");
+    }
+
+    @Override
+    public void start() throws Exception {
+        System.out.println("starting ...");
+        main(null);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("stopping ...");
+        if (server != null) {
+            server.stop();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("done.");
+        server = null;
+    }
+}
