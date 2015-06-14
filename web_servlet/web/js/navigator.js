f86166ae19041f9d867799a4393da34101b36207
zethra <jediben97@gmail.com>
2015-06-14 15:51:17 -0400

    Added reverse button, scrolling buttons, and more

--- web_servlet/web/js/navigator.js
+++ web_servlet/web/js/navigator.js
@@ -1,48 +1,95 @@
-var rooms;
+var $start;
+var $end;
 
-$.getJSON("js/rooms.json", function(data) {
-    rooms = data;
-    var options = {
-        plugins: ['restore_on_backspace'],
-        persist: false,
-        maxItems: 1,
-        valueField: 'name',
-        labelField: 'name',
-        searchField: 'name',
-        options: rooms
-    };
-    
-    $('#start').selectize(options);
-    $('#end').selectize(options);
-});
+function loadRooms() {
+    $.getJSON("js/rooms.json", function(data) {
+        var rooms = data;
+        var options = {
+            plugins: ['restore_on_backspace'],
+            persist: false,
+            maxItems: 1,
+            valueField: 'name',
+            labelField: 'name',
+            searchField: 'name',
+            options: rooms
+        };
 
-$('#sumbit').click(function() {
+        $start = $('#start').selectize(options);
+        $end = $('#end').selectize(options);
+    });
+}
+
+function getData(f) {
     $.ajax({url: "navigator.html",
         data: {
             "start": $('#start').val(),
             "end": $('#end').val()
         },
         success: function (data, textStatus, jqXHR) {
-            $('#output').html(data);
+            console.log(data);
+            f(data);
         }
     });
+}
+
+function scroll(id) {
     $('html, body').stop().animate({
-                scrollTop: $($('a[href=#directions]').attr('href')).offset().top
+                scrollTop: $($('a[href=' + id +']').attr('href')).offset().top
             }, 1500, 'easeInOutExpo');
-});
+}
 
-$('#start').on("keyup keypress", function(e) {
-  var code = e.keyCode || e.which; 
-  if (code  === 13) {               
-    e.preventDefault();
-    return false;
-  }
-});
+function output(data) {
+    $('#output').html(data);
+}
 
-$('#end').on("keyup keypress", function(e) {
-  var code = e.keyCode || e.which; 
-  if (code  === 13) {               
-    e.preventDefault();
-    return false;
-  }
+function reverse() {
+    var start = $('#start').val();
+    var end = $('#end').val();
+    if(start !== "" && end !== "") {
+        $('#start').val(end);
+        $('#end').val(start);
+        $start[0].selectize.setValue(end);
+        $end[0].selectize.setValue(start);
+        getData(output);
+    } else {
+        output("Please enter a start and end room");
+        scroll("#getdirections");
+    }
+}
+
+$(document).ready(function() {
+    loadRooms();
+    $('#sumbit').click(function() {
+        getData(output);
+        scroll("#directions");
+    });
+    
+    $('#reverse').click(function() {
+        reverse();
+    });
+    
+    $('#toDir').click(function() {
+        scroll("#getdirections");
+        console.log("top");
+    });
+    
+    $('#toTop').click(function() {
+        scroll("#page-top");
+    });
+    
+    $('#start').on("keyup keypress", function(e) {
+        var code = e.keyCode || e.which; 
+        if (code  === 13) {               
+          e.preventDefault();
+          return false;
+        }
+    });
+
+      $('#end').on("keyup keypress", function(e) {
+        var code = e.keyCode || e.which; 
+        if (code  === 13) {               
+          e.preventDefault();
+          return false;
+        }
+    });
 });
